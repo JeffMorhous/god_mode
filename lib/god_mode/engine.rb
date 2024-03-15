64cee49d38fb65f6ec65dc49966256366b067364
Jeff Morhous <morhous.jeff@gmail.com>
2024-03-14 20:43:51 -0400

    Update the engine.rb

--- lib/god_mode/engine.rb
+++ lib/god_mode/engine.rb
@@ -1,5 +1,8 @@
 module GodMode
   class Engine < ::Rails::Engine
     isolate_namespace GodMode
+  #   initializer "god_mode.assets.precompile" do |app|
+  #     app.config.assets.precompile += %w( application.js application.css )
+  #   end
   end
 end
